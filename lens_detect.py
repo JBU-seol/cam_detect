baae69316d2fa72d8d1b43acfe70876136b388f2
JBU-seol <48273159+JBU-seol@users.noreply.github.com>
2019-12-11 19:54:57 +0900

    Add introduction & manual

--- lens_detect.py
+++ lens_detect.py
@@ -11,34 +11,58 @@ import bcrypt
 class Detect(QWidget):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("Cam_Test")
+        self.setWindowTitle("Screen Guarder")
         self.setWindowIcon(QIcon("icon.png"))
         self.setGeometry(150, 150, 650, 540)
-        self.initUI();
+        self.setFixedSize(650,540)
+        self.initUI()
         finish = QAction("Quit", self)
         finish.triggered.connect(self.closeEvent)
 
     def initUI(self):
+        self.introduction = open("introduction.txt","r").read()
+        self.manual = open("manual.txt","r").read()
         self.cpt = cv2.VideoCapture(0)
         self.fps = 24
         self.sens = 300
-        #_, self.img_o = self.cpt.read()
-        #cv2.imwrite('img_o.jpg', self.img_o)
+
+        self.mainframe = QLabel(self.introduction,self)
+        self.mainframe.resize(400,480)
+        self.mainframe.setScaledContents(True)
+        self.mainframe.move(125,5)
 
         self.frame = QLabel(self)
-        self.frame.resize(640, 480)
+        self.frame.resize(100, 100)
         self.frame.setScaledContents(True)
-        self.frame.move(5, 5)
+        self.frame.setPixmap(QPixmap('icon.png'))
+        self.frame.move(500, 5)
+
+        self.btn_intro = QPushButton("소개", self)
+        self.btn_intro.resize(100, 25)
+        self.btn_intro.move(5, 25)
+        self.btn_intro.clicked.connect(self.intro)
+
+        self.btn_manual = QPushButton("매뉴얼", self)
+        self.btn_manual.resize(100, 25)
+        self.btn_manual.move(5, 75)
+        self.btn_manual.clicked.connect(self.showmanual)
 
         self.btn_on = QPushButton("켜기", self)
         self.btn_on.resize(100, 25)
-        self.btn_on.move(5, 490)
+        self.btn_on.move(5, 125)
         self.btn_on.clicked.connect(self.start)
 
-        self.btn_on = QPushButton("끄기", self)
-        self.btn_on.resize(100, 25)
-        self.btn_on.move(5+100+5, 490)
-        self.btn_on.clicked.connect(self.stop)
+        self.btn_off = QPushButton("끄기", self)
+        self.btn_off.setEnabled(False)
+        self.btn_off.resize(100, 25)
+        self.btn_off.move(5, 175)
+        self.btn_off.clicked.connect(self.stop)
+
+        self.btn_end = QPushButton("종료", self)
+        self.btn_end.resize(100, 25)
+        self.btn_end.move(5, 490)
+        self.btn_end.clicked.connect(self.end)
+
 
         self.prt = QLabel(self)
         self.prt.resize(200, 25)
@@ -48,10 +72,19 @@ class Detect(QWidget):
         self.prt1.resize(200, 25)
         self.prt1.move(5+105+105+200, 490)
 
+    def intro(self):
+        self.mainframe.setText(self.introduction)
+
+    def showmanual(self):
+        self.mainframe.setText(self.manual)
+
+    def end(self):
+        sys.exit(0)
+
     def faceDetect(self):
-        playsound('1.mp3', False)
         self.prt.setText("얼굴 탐지하였습니다.")
         '''
+        playsound('1.mp3', False)
         login = Login()
         if login.exec_() == QtWidgets.QDialog.Accepted:
             window = Window()
@@ -71,9 +104,11 @@ class Detect(QWidget):
             sys.exit(app.exec_())
 
     def start(self):
+        self.btn_off.setEnabled(True)
         self.timer = QTimer()
+        self.timer.start()
+        self.timer.setInterval(100)
         self.timer.timeout.connect(self.nextFrameSlot)
-        self.timer.start(int(1000./self.fps))
 
     def nextFrameSlot(self):
         face_cascade = cv2.CascadeClassifier('haarcascade_frontface.xml')
@@ -87,7 +122,6 @@ class Detect(QWidget):
         else:
             self.faceUnDetect()
 
-        #line모두 그리기
         ret, image_binary = cv2.threshold(cam, 127, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)
         contour, hierarcy = cv2.findContours(image_binary, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
 
@@ -97,25 +131,28 @@ class Detect(QWidget):
             x, y, w, h = cv2.boundingRect(c)
 
             size = len(approx)
-            if size == 4 and 2000 < peri:
+            if size == 4 and 2000 < peri: #print(peri)#2236
                 cv2.imwrite("Evidence.jpg", cam_real)
                 warning = cv2.imread("warning1.jpg", cv2.IMREAD_COLOR)
                 cv2.namedWindow("Warning!", cv2.WND_PROP_FULLSCREEN)
                 cv2.setWindowProperty("Warning!", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
                 cv2.imshow("Warning!", warning)
                 self.camDetect()
-                #print(peri)#2236
-            cv2.line(cam_color, tuple(approx[0][0]), tuple(approx[size - 1][0]), (0, 255, 0), 3)
-            for k in range(size - 1):
-                cv2.line(cam_color, tuple(approx[k][0]), tuple(approx[k + 1][0]), (0, 255, 0), 3)
+        #     Line 그리기
+        #     cv2.line(cam_color, tuple(approx[0][0]), tuple(approx[size - 1][0]), (0, 255, 0), 3)
+        #     for k in range(size - 1):
+        #         cv2.line(cam_color, tuple(approx[k][0]), tuple(approx[k + 1][0]), (0, 255, 0), 3)
 
         img = QImage(cam_color, cam_real.shape[1], cam_real.shape[0], QImage.Format_RGB888)
         pix = QPixmap.fromImage(img)
         self.frame.setPixmap(pix)
 
     def stop(self):
-        self.frame.setPixmap(QPixmap.fromImage(QImage()))
-        self.timer.stop()
+        if self.timer.isActive():
+            self.frame.setPixmap(QPixmap('icon.png'))
+            self.timer.stop()
+            self.btn_off.setEnabled(False)
+
 
     def closeEvent(self, event):
         close = QMessageBox.question(self, "안내", "종료할 수 없습니다.", QMessageBox.Ok)
